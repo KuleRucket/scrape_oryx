2a8604b56b67d311f607fd99ba5ad065f9c34ca9
Luke Tucker <you@example.com>
2024-11-22 10:37:57 +0100

    switch to logger

--- scrape_oryx.R
+++ scrape_oryx.R
@@ -13,8 +13,6 @@ if (Sys.info()["sysname"] == "Linux") {
   print("Setting Staged Install to False")
 }
 
-message("Init libraries")
-
 library(renv)
 renv::restore(prompt = FALSE)
 library(rvest)
@@ -30,39 +28,40 @@ library(glue)
 library(ggplot2)
 library(scales)
 library(ggthemes)
+library(logger)
 
 source("R/functions.R")
 source("R/scrape_data.R")
 source("R/totals_by_type.R")
 source("R/daily_count.R")
 
-message("starting")
+log_info("starting")
 
 russia_url <- "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html"
 ukraine_url <- "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html"
 
 today <- format(Sys.Date(), "%Y-%m-%d")
 
-message("updating totals_by_system.csv (this takes ages)")
+log_info("updating totals_by_system.csv (this takes ages)")
 totals_by_system <- create_data() %>%
   readr::write_csv(., file = glue::glue("outputfiles/totals_by_system.csv"))
 
-message("updating totals_by_system_wide.csv")
+log_info("updating totals_by_system_wide.csv")
 totals_by_system_wide <- total_by_system_wide(totals_by_system) %>%
   readr::write_csv(.,
                    file = glue::glue("outputfiles/totals_by_system_wide.csv"))
 
-message("updating total_by_type.csv")
+log_info("updating total_by_type.csv")
 total_by_type <- totals_by_type() %>%
   readr::write_csv(., file = glue::glue("outputfiles/totals_by_type.csv"))
 
-message("updating daily_count.csv")
+log_info("updating daily_count.csv")
 daily_count <- daily_count() %>%
   readr::write_csv(., file = "outputfiles/daily_count.csv")
 
-message("quarto_render")
+log_info("quarto_render")
 
 #rmarkdown::render("index.Rmd")
 quarto::quarto_render("index.qmd")
 
-message("done")
+log_info("done")
